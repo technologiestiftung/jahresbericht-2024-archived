570534c2fef73c68a623e2002db0b91932eed968
Luise Brandenburger <luise.brandenburger@ts.berlin>
2024-10-29 18:08:52 +0100

    feat: add mobile view & update semantics

--- src/sections/Main2024/index.jsx
+++ src/sections/Main2024/index.jsx
@@ -1,12 +1,12 @@
-import cn from "./Main2024.module.scss";
 import NewIcons from "../../components/NewIcons";
+import cn from "./Main2024.module.scss";
 
 function MainSection({ content, icon }) {
   const { title, text } = content;
   return (
     <section className={cn.wrapper} id={icon}>
       <div className={cn.content}>
-        <div>{!!icon && <NewIcons type={icon} />}</div>
+        {icon && <div>{!!icon && <NewIcons type={icon} />}</div>}
         <div>
           <h2 className={cn.title}>{title}</h2>
           <p className={cn.subTitle}>{text}</p>
